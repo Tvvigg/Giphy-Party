fc9b80dc2539b1ce5314079fb55c2fc72c12f5e8
Alex <aengel38@gmail>
2022-03-24 02:17:09 -0700

    updateCss

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("Let's get this party started!");
+const $gifArea = $("#gif-area");
+const $term = $("#term");
+
+//Retrives gif from giphy API and appends to gif area div
+function updateGifs(res) {
+  //Gets length of results
+  let resultTotal = res.data.length;
+  if (resultTotal) {
+    //finds a random index of result total
+    let random = Math.floor(Math.random() * resultTotal);
+    //creates new gif element using random index
+    let $newGif = $("<img>", {
+      src: res.data[random].images.original.url,
+      class: "gif",
+    });
+    //appends new gif to gif area div
+    $gifArea.append($newGif);
+  }
+}
+
+//Get search term from the form;
+$("form").on("submit", async function (evt) {
+  evt.preventDefault();
+
+  let term = $term.val();
+  $term.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: term,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym",
+    },
+  });
+  updateGifs(response.data);
+});
+
+//Remove all gifs from the page
+$("#remove").on("click", function () {
+  $("#gif-area").empty();
+});
